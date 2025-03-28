f723f0c7e506cb853e25a3088dbd1fdb12527a9c
ajsorrells <167811075+ajsorrells@users.noreply.github.com>
2025-03-28 00:04:54 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+const city = "New York";
+console.log(city);
+
+//city = "Los Angeles"
+
+const age = 25;
+console.log(age);
+
+var pet = "dog";
+var pet = "cat";
+console.log(pet);
+
+fruit = "Apple";
+fruit = "Banana";
+console.log(fruit);
+
+const country = "USA";
+//country = "Canada";
+console.log(country);
+
+//console.log(drink);
+//var drink = "Water";
+//console.log(food);
+
+const food = "Pizza";
+console.log(food);
+
+const birthYear = 2000;
+currentYear = 2026;
+//birthYear = 2001;
+let currentYear = 2025;
+console.log(`I was born in ${birthYear} and now it's ${currentYear}.`);
+
+const favoriteColor = "Blue";
+console.log(favoriteColor)
